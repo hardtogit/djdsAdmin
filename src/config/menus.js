6c847a65bd4e7f32d542a5e1a3d58c17366040e6
xiangguo <guo.xiang@56qq.com>
2019-12-02 18:13:34 +0800

    简化模板

--- src/config/menus.js
+++ src/config/menus.js
@@ -1,41 +1,8 @@
 export default [{
-    title: '赛事管理',
-    key: 'gameManage',
-    name: 'gameManage',
-    path: '/gameManage',
-    icon: 'trophy'
-  },
-  {
-    title: 'Banner管理',
-    key: 'bannerManage',
-    name: 'bannerManage',
-    path: '/bannerManage',
-    icon: 'picture'
-  },{
-    title: '资讯管理',
-    key: 'informationManage',
-    name: 'informationManage',
-    path: '/informationManage',
-    icon: 'profile'
-  },{
     title: '用户管理',
     key: 'userManage',
     name: 'userManage',
     path: '/userManage',
     icon: 'user'
-  },
-  {
-    title: '礼物管理',
-    key: 'giftManage',
-    name: 'giftManage',
-    path: '/giftManage',
-    icon: 'shop'
-  },
-  {
-    title: '积分榜管理',
-    key: 'scoreManage',
-    name: 'scoreManage',
-    path: '/scoreManage',
-    icon: 'profile'
   }
 ];
